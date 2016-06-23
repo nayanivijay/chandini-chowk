5f4bde9418fd3a398087b41c6b27d8ebdd01b57a
chaynika <chaynika@amagi.com>
2016-06-23 15:15:25 +0530

    Fixed permission denied issue

--- features/support/mysql_dumps.sh
+++ features/support/mysql_dumps.sh
@@ -4,7 +4,7 @@ USER=$1
 PASSWORD=$2
 DBNAME=dsp
 tables="user permission role role_permission user user_role"
-OUTPUT_DIR="/var/tmp/chandni-chowk-db-dumps"
+OUTPUT_DIR="$HOME/chandni-chowk-db-dumps"
 FILE_PREFIX=${DBNAME}
 FILE_EXT='.sql'
 FILE_NAME=${FILE_PREFIX}${FILE_EXT}
@@ -15,24 +15,21 @@ then
         exit 1
 fi
 
-mkdir -p ${OUTPUT_DIR}
+sudo mkdir -p ${OUTPUT_DIR}
+sudo chmod 777 ${OUTPUT_DIR}
 mysqldump -u$USER -p$PASSWORD ${DBNAME} > ${OUTPUT_DIR}/${FILE_NAME}
 mysql -u$USER -p${PASSWORD} ${DBNAME} < ${OUTPUT_DIR}/${FILE_NAME}
 
 for table in $tables
 do
         filename=${table}.sql
-        mysqldump -u$USER -p${PASSWORD} $DBNAME $table > ${OUTPUT_DIR}/$filename
-        if [[ $? -ne 0 ]]
+        sudo mysqldump -u$USER -p${PASSWORD} $DBNAME $table > ${OUTPUT_DIR}/$filename
+        if [ $? -ne 0 ]
         then
                 echo "$db $table dump unsuccessful"
 		exit 1
         fi
 done
-
-pushd ${OUTPUT_DIR}
-#rm ${FILE_NAME}
-popd
 
 exit 0
 
