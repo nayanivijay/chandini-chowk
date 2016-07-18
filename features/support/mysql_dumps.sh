8aef34de4ca6e6e1b54b02ec906494c8582b16c0
chaynika <chaynika@amagi.com>
2016-07-18 15:44:04 +0530

    Added host name

--- features/support/mysql_dumps.sh
+++ features/support/mysql_dumps.sh
@@ -2,6 +2,7 @@
 
 USER=$1
 PASSWORD=$2
+MYSQL_HOST=$3
 DBNAME=dsp
 tables="user permission role role_permission user user_role"
 OUTPUT_DIR="$HOME/chandni-chowk-db-dumps"
@@ -9,7 +10,7 @@ FILE_PREFIX=${DBNAME}
 FILE_EXT='.sql'
 FILE_NAME=${FILE_PREFIX}${FILE_EXT}
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
         echo "Usage: mysql_dumps <mysql_user> <mysql_pass>"
         exit 1
@@ -17,13 +18,13 @@ fi
 
 sudo mkdir -p ${OUTPUT_DIR}
 sudo chmod 777 ${OUTPUT_DIR}
-mysqldump -u$USER -p$PASSWORD ${DBNAME} > ${OUTPUT_DIR}/${FILE_NAME}
-mysql -u$USER -p${PASSWORD} ${DBNAME} < ${OUTPUT_DIR}/${FILE_NAME}
+mysqldump -u$USER -p$PASSWORD -h $MYSQL_HOST ${DBNAME} > ${OUTPUT_DIR}/${FILE_NAME}
+mysql -u$USER -p${PASSWORD} -h $MYSQL_HOST ${DBNAME} < ${OUTPUT_DIR}/${FILE_NAME}
 
 for table in $tables
 do
         filename=${table}.sql
-        sudo mysqldump -u$USER -p${PASSWORD} $DBNAME $table > ${OUTPUT_DIR}/$filename
+        sudo mysqldump -u$USER -p${PASSWORD} -h $MYSQL_HOST $DBNAME $table > ${OUTPUT_DIR}/$filename
         if [ $? -ne 0 ]
         then
                 echo "$db $table dump unsuccessful"
